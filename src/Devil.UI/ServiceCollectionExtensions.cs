e6b3de61026c5b34a057ae94373e4bae6b9b9078
Jeffrey Ladd <jladd@Jeffreyladd.net>
2023-07-27 20:23:42 -0400

    Adds Skeleton Base

---
+++ src/Devil.UI/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Devil.UI;
+
+[ExcludeFromCodeCoverage]
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Extension Method to setup all the TPIE.UI Dependencies.
+    /// </summary>
+    /// <param name="services">The Service Collection for DI.</param>
+    /// <param name="config">The configuration.</param>
+    /// <returns></returns>
+    public static IServiceCollection SetupDevilUI(this IServiceCollection services)
+        => services.AddBlazorise(options =>
+        {
+            options.Immediate = true;
+        })
+        .Replace(ServiceDescriptor.Transient<IComponentActivator, ComponentActivator>())
+        .AddBootstrap5Providers()
+        .AddFontAwesomeIcons();
+}
---
+++ src/Devil.Web/Client/Program.cs
@@ -0,0 +1,13 @@
+using Devil.UI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Main>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.SetupDevilUI();
+
+await builder.Build().RunAsync();
